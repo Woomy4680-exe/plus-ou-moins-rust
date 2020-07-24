02677c2852e8d1900f4603bca26fe4bf4abbb1dd
Woomy4680-exe <matzar1808@gmail.com>
2020-07-24 10:53:59 +0200

    Base game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use std::io; //For get STDIN
+use rand::prelude::*;  //For generateRandom
 fn main() {
-    println!("Hello, world!");
+    println!("Plus ou moins");
+    game();
+}
+fn game() {
+    let num = gen_number(); 
+    let mut input = prompt_num();
+    loop {
+     
+        if input == num {
+            println!("Bravo! Vous avez gagné!");
+            exit(false);
+        } else if input > num {
+            println!("Votre nombre est trop grand!");
+            input = prompt_num();
+        } else {
+            println!("Votre nombre est trop petit!");
+            input = prompt_num();
+        }
+    } 
+}
+fn gen_number() -> i32 {
+    let n = rand::thread_rng().gen_range(1, 50);
+    return n; 
+}
+fn prompt_num() -> i32 {
+    let mut num = String::new();
+    println!("Veuillez entrer un nombre!");
+    match io::stdin().read_line(&mut num) {
+        Ok(_n) => {
+        } 
+        Err(e) => {
+            println!("Erreur lors de la lecture du nombre: {:?}", e); 
+            exit(true);
+        }
+    }
+    let finalnum;
+   
+    match num.trim().parse::<i32>() {
+        Ok(n) => {
+            finalnum = n;
+        }
+        Err(_e) =>  {
+            println!("Merci d'entrer un nombre valide!");
+            finalnum = 0;
+            exit(true); 
+        }
+    };
+    return finalnum; 
+}
+fn exit(err: bool) {
+    println!(""); 
+    println!("Merci d'avoir joué!"); 
+    println!("\n====================");
+    if err {
+        std::process::exit(1);
+    } else {
+        std::process::exit(0);
+    }
 }

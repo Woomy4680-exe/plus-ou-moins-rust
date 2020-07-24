45bea88178b0ab8ea6d1ec5fbb49db90a9239061
Woomymy <github@woomy.ovh>
2020-07-24 18:44:58 +0200

    welcome message

--- src/lib/term.rs
+++ src/lib/term.rs
@@ -1,4 +1,19 @@
-mod term;
+use console::Term; 
 /*
 Terminal manager(clear, colorprint)    
 */
+pub fn clear() {
+    let terminal = Term::stdout();
+    match terminal.clear_screen() {
+        Ok(_n) => {
+
+        }
+        Err(_e) => {
+            println!("");
+            println!("");
+            println!("");
+            println!("");
+            //Clean du pauvre
+        }
+    }
+}
--- src/lib/util.rs
+++ src/lib/util.rs
@@ -1,4 +1,12 @@
-mod gameutils; 
+use console::style; 
 /*
 All utils(exit process,...)    
 */
+
+// Prints welcome message
+pub fn hello() {
+    println!("{}", style("============================").cyan()); 
+    println!("{}", style("Jeu plus ou Moins en Rust").green());
+    println!("{}", style("Par Woomy4680-exe").green()); 
+    println!("{}", style("============================").cyan()); 
+}
